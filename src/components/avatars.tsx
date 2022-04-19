6fbc75bd82c1b0262e22e0212c765300f46a58b3
felipe2640 <felipe_brito09@hotmail.com>
2022-04-19 11:30:58 -0300

    fix(Identify Keys inside the array): Add id in array.

--- src/components/avatars.tsx
+++ src/components/avatars.tsx
@@ -1,25 +1,30 @@
 const avatars = [
     {
+      id:1,
       alt: 'Casa',
       url: 'https://svgsilh.com/svg/148837.svg',
       icon: 'bi:house-fill'
     },
     {
+      id:2,
       alt: 'Auto',
       url: 'https://svgsilh.com/svg/1889366.svg',
       icon: `ion:car-sport-sharp`
     },
     {
+      id:3,
       alt: 'Reforma',
       url: 'https://bit.ly/prosper-baba',
       icon: `uil:constructor`
     },
     {
+      id:4,
       alt: 'Vida',
       url: 'https://bit.ly/code-beast',
       icon: `bi:heart-pulse`
     },
     {
+      id:5,
       alt: 'Previdência',
       url: 'https://bit.ly/code-beast',
       icon: `carbon:pedestrian-family`
--- src/components/company.ts
+++ src/components/company.ts
@@ -2,72 +2,50 @@ import { Companys } from '../interface';
 
 const companyArray: Array<Companys> = [
   {
+    id:1,
     empresa: 'Porto seguros',
     tipo: 'Seguro',
     color: 'blue',
     logo1: '/porto-seguros-logo.png',
-    logo: [
-      {
-        lg1: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fselectra.net.br%2Fseguros%2Fseguradoras%2Fporto-seguro-seguros%2Ftelefone&psig=AOvVaw0LebyIhnHlteUX255cwus_&ust=1649249935947000&source=images&cd=vfe&ved=0CAcQjRxqFwoTCID40_v8_PYCFQAAAAAdAAAAABADhttps://www.google.com/url?sa=i&url=https%3A%2F%2Fselectra.net.br%2Fseguros%2Fseguradoras%2Fporto-seguro-seguros%2Ftelefone&psig=AOvVaw0LebyIhnHlteUX255cwus_&ust=1649249935947000&source=images&cd=vfe&ved=0CAcQjRxqFwoTCID40_v8_PYCFQAAAAAdAAAAABAD',
-        lg2: 'https://svgsilh.com/svg/148837.svg'},
-    ]
+    
   },
   {
+    id:2,
     empresa: 'AIG',
     tipo: 'Seguro',
     color: 'blue',
     logo1: '/AIG_logo.png',
-    logo: [
-        {
-          lg1: 'https://svgsilh.com/svg/148837.svg',
-          lg2: 'https://svgsilh.com/svg/148837.svg'},
-      ]
+    
   },
-  {
+  { 
+    id:3,
     empresa: 'Capemisa',
     tipo: 'Seguro',
     color: 'orange',
     logo1: '/Capemisa_logo.jpg',
-    logo: [
-        {
-          lg1: 'https://svgsilh.com/svg/148837.svg',
-          lg2: 'https://svgsilh.com/svg/148837.svg'},
-      ]
   },
   {
+    id:4,
     empresa: 'Mongeral Aegon',
     tipo: 'Seguro',
     color: 'blue',
     logo1: '/Mongeral_Aegon.png',
-    logo: [
-        {
-          lg1: 'https://svgsilh.com/svg/148837.svg',
-          lg2: 'https://svgsilh.com/svg/148837.svg'},
-      ]
   },
   {
+    id:5,
     empresa: 'Embracon',
     tipo: 'Consórcio',
     color: 'red',
     logo1: '/embracon_logo.jpg',
     googleSearch: 'V3TnGO5jtUvihV6KSQONBtw_Z_x3b6yWhLwjiTlXeE8',
     content:'Consórcio Embracon - Simulador de Consórcio Online - Carta de Crédito para Imóveis, Carros, Motos ou Serviços. Clique Aqui e Confira!',
-    logo: [
-        {
-          lg1: 'https://svgsilh.com/svg/148837.svg',
-          lg2: 'https://svgsilh.com/svg/148837.svg'},
-      ]
   },
   {
+    id:6,
     empresa: 'Bancorbrás',
     tipo: 'Consórcio',
     color: 'blue',
     logo1: '/bancorbras.webp',
-    logo: [
-        {
-          lg1: 'https://svgsilh.com/svg/148837.svg',
-          lg2: 'https://svgsilh.com/svg/148837.svg'},
-      ]
   }
 ];
 export default companyArray;
